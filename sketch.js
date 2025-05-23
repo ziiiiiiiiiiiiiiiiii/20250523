4797735925aa22154386aedd5b790dff5ab29b5a
ziiiiiiiiiiiiiiiiii <fve1981@gmail.com>
2025-05-23 10:53:54 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,7 +3,6 @@ let predictions = [];
 const lipIndices = [409,270,269,267,0,37,39,40,185,61,146,91,181,84,17,314,405,321,375,291];
 
 function setup() {
-  // 置中畫布
   let cnv = createCanvas(640, 480);
   cnv.style('display', 'block');
   cnv.parent(document.body);
@@ -26,18 +25,21 @@ function setup() {
 function draw() {
   background(220);
 
-  // 畫出嘴唇線條
   if (predictions.length > 0) {
     let keypoints = predictions[0].scaledMesh;
     stroke(255, 0, 0);
-    strokeWeight(15);
+    strokeWeight(15); // 更粗
     noFill();
-    beginShape();
-    for (let i = 0; i < lipIndices.length; i++) {
-      let idx = lipIndices[i];
-      let [x, y] = keypoints[idx];
-      vertex(x, y);
+    for (let i = 0; i < lipIndices.length - 1; i++) {
+      let idxA = lipIndices[i];
+      let idxB = lipIndices[i + 1];
+      let [x1, y1] = keypoints[idxA];
+      let [x2, y2] = keypoints[idxB];
+      line(x1, y1, x2, y2);
     }
-    endShape(CLOSE);
+    // 收尾，連接最後一點到第一點
+    let [xStart, yStart] = keypoints[lipIndices[0]];
+    let [xEnd, yEnd] = keypoints[lipIndices[lipIndices.length - 1]];
+    line(xEnd, yEnd, xStart, yStart);
   }
 }
